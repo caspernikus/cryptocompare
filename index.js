4ed24c696ff3221f54df9f7daa7e09ddc07e1d9d
Benedikt Veith <bv@scherer-software.de>
2018-02-16 14:18:38 +0100

    part 2

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Hello World');
+var fs = require('fs');
+var Mustache = require('Mustache');
+
+const coins = [
+    {
+        name: 'Bitcoin',
+        price: '10000 $'
+    }, {
+        name: 'Ethereum',
+        price: '900 $'
+    }, {
+        name: 'Ripple',
+        price: '90 $'
+    }
+];
+
+fs.readFile('templates/main.html', 'utf8', function (err, content) {
+  if (err) throw err;
+
+  const data = {
+      coin_rows: coins
+  };
+  const renderedMain = Mustache.render(content, data);
+
+  writeToDisk(renderedMain);
+});
+
+function writeToDisk(content) {
+    fs.writeFile('out/out.html', content, function (err) {
+      if (err) throw err;
+
+      console.log('Done saving output!');
+    });
+}
