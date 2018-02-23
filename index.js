9b062eda29af598eee179154f217015af9e6c333
Benedikt Veith <bv@scherer-software.de>
2018-02-23 14:47:58 +0100

    generate beautifuler template

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 var fs = require('fs');
 var Mustache = require('Mustache');
+var request = require('request');
 
-const coins = [
-    {
-        name: 'Bitcoin',
-        price: '10000 $'
-    }, {
-        name: 'Ethereum',
-        price: '900 $'
-    }, {
-        name: 'Ripple',
-        price: '90 $'
-    }
-];
+getTop10Coins();
+function getTop10Coins() {
+    request('https://api.coinmarketcap.com/v1/ticker/?limit=10', function (error, response, body) {
+        if (error) {
+            console.log('error:', error);
+        }
 
-fs.readFile('templates/main.html', 'utf8', function (err, content) {
-  if (err) throw err;
+        readTemplate(body);
+    });
+}
 
-  const data = {
-      coin_rows: coins
-  };
-  const renderedMain = Mustache.render(content, data);
+function readTemplate(coins) {
+    fs.readFile('templates/main.html', 'utf8', function (err, content) {
+      if (err) throw err;
 
-  writeToDisk(renderedMain);
-});
+      const data = {
+          coin_rows: JSON.parse(coins),
+          returnImage: function() {
+              if (this.symbol == "XLM") {
+                  return 'STR';
+              }
+
+              return this.symbol;
+          },
+          getPrice: function() {
+              return parseFloat(this.price_usd).toFixed(2);
+          },
+          getColor1h: function() {
+              if (parseFloat(this.percent_change_1h) < 0) {
+                  return 'text-danger';
+              }
+
+              return 'text-success';
+          },
+          getColor7d: function() {
+              if (parseFloat(this.percent_change_7d) < 0) {
+                  return 'text-danger';
+              }
+
+              return 'text-success';
+          },
+          getColor24h: function() {
+              if (parseFloat(this.percent_change_24h) < 0) {
+                  return 'text-danger';
+              }
+
+              return 'text-success';
+          }
+      };
+      const renderedMain = Mustache.render(content, data);
+
+      writeToDisk(renderedMain);
+    });
+}
 
 function writeToDisk(content) {
     fs.writeFile('out/out.html', content, function (err) {
